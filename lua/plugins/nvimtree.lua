403bd8234ea82610c2a26a6b1c11f2230fe1e80b
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2023-04-20 17:48:44 +0800

    Remove NvimTree mappings

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,20 +1,21 @@
-
-local function on_attach(bufnr)
-    local api = require("nvim-tree.api")
-
-    local function opts(desc)
-        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, nowait = true }
-    end
-
-    keymap('n', '<CR>', api.node.open.edit, opts('Open'))
-    keymap('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
-    keymap('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
-    keymap('n', 'r', api.fs.rename, opts('Rename'))
-    keymap('n', 'R', api.tree.reload, opts('Refresh'))
-    keymap('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
-    keymap('n', 'S', api.tree.search_node, opts('Search'))
-
-end
+--local opts = { noremap = true, silent = true }
+--local keymap = vim.api.nvim_set_keymap
+--
+--local function on_attach(bufnr)
+--    local api = require("nvim-tree.api")
+--
+--    local function opts(desc)
+--        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, nowait = true }
+--    end
+--
+--    keymap('n', '<CR>', api.node.open.edit, opts('Open'))
+--    keymap('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+--    keymap('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+--    keymap('n', 'r', api.fs.rename, opts('Rename'))
+--    keymap('n', 'R', api.tree.reload, opts('Refresh'))
+--    keymap('n', 'U', api.tree.toggle_custom_filter, opts('Toggle Hidden'))
+--    keymap('n', 'S', api.tree.search_node, opts('Search'))
+--end
 
 require("nvim-tree").setup {
     renderer = {
